0baba4479f5662cf7aed18083f937df0c8b4ff42
Michael Peddycord <michaelpeddycord@uncc.edu>
2025-04-08 21:43:02 -0400

    Flask Part 3

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template, redirect, url_for
 import sqlalchemy as sa
 from app import db
 from app.main.forms import ContactForm
-from app.models import User
+from app.models import Post, Movie
 from app.main import bp
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    middle_movies_1 = Movie.query.where(Movie.position == 'middle').limit(3)
+    middle_movies_2 = Movie.query.where(Movie.position == 'middle').offset(3).limit(2)
+    middle_movies_3 = Movie.query.where(Movie.position == 'middle').offset(5).limit(3)
+    bottom_movies_1 = Movie.query.where(Movie.position == 'bottom').limit(4)
+    bottom_movies_2 = Movie.query.where(Movie.position == 'bottom').offset(4).limit(4)
+    return render_template('index.html', middle_movies_1=middle_movies_1, middle_movies_2=middle_movies_2, middle_movies_3=middle_movies_3, bottom_movies_1=bottom_movies_1, bottom_movies_2=bottom_movies_2)
 
 @bp.route('/contact', methods=['GET', 'POST'])
 def contact():
@@ -25,24 +30,36 @@ def contact():
         inquiry_type = form.inquiry_type.data
         message = form.message.data
         return redirect(url_for('main.index'))
-    return render_template('contact.html', form=form, name=name, email=email, subject=subject, inquiry_type=inquiry_type, message=message, title=title)
+    bottom_movies_1 = Movie.query.where(Movie.position == 'bottom').limit(4)
+    return render_template('contact.html', form=form, name=name, email=email, subject=subject, inquiry_type=inquiry_type, message=message, title=title, bottom_movies_1=bottom_movies_1)
 
 @bp.route('/blog')
 def list():
     title = 'Blog & Article'
-    user = db.first_or_404(sa.select(User))
-    return render_template('blog-list.html', user=user, title=title)
+    other_movies = Movie.query.where(Movie.position == 'other').limit(3)
+    posts = Post.query.order_by(Post.date_created.desc()).all()
+    bottom_movies_1 = Movie.query.where(Movie.position == 'bottom').limit(4)
+
+    return render_template('blog-list.html', posts=posts, title=title, other_movies=other_movies, bottom_movies_1=bottom_movies_1)
 
 @bp.route('/about')
 def about():
     title = 'About Us'
-    return render_template('about-us.html', title=title)
+    bottom_movies_1 = Movie.query.where(Movie.position == 'bottom').limit(4)
+
+    return render_template('about-us.html', title=title, bottom_movies_1=bottom_movies_1)
 
 @bp.route('/post/<id>')
 def post(id):
-    return render_template('blog-page.html', id=id)
+    post = Post.query.get(id)
+    bottom_movies_1 = Movie.query.where(Movie.position == 'bottom').limit(4)
+
+    return render_template('blog-page.html', id=id, post=post, bottom_movies_1=bottom_movies_1)
 
 @bp.route('/movie/<id>')
 def movie(id):
     title = 'Movie Details'
-    return render_template('movie-details.html', id=id, title=title)
+    movie = Movie.query.get(id)
+    other_movies = Movie.query.where(Movie.position == 'other').limit(3)
+    bottom_movies_1 = Movie.query.where(Movie.position == 'bottom').limit(4)
+    return render_template('movie-details.html', id=id, title=title, movie=movie, other_movies=other_movies, bottom_movies_1=bottom_movies_1)
--- app/models.py
+++ app/models.py
@@ -2,12 +2,40 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from datetime import date
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(255), index=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(255), index=True)
     password: so.Mapped[Optional[str]] = so.mapped_column(sa.String(255))
+    avatar_url: so.Mapped[str] = so.mapped_column(sa.String(255), index=True)
 
     def __repr__(self):
         return '<User {}'.format(self.name)
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    title: so.Mapped[str] = so.mapped_column(sa.String(255), index=True)
+    image_url: so.Mapped[str] = so.mapped_column(sa.String(255), index=True)
+    stub: so.Mapped[str] = so.mapped_column(sa.String(255), index=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(2000))
+    date_created: so.Mapped[date] =  so.mapped_column(sa.Date)
+    author_id = sa.Column(sa.Integer, sa.ForeignKey('User.id'), nullable=False)
+    author = so.relationship("User", foreign_keys=[author_id], primaryjoin="User.id == Post.author_id")
+
+class Movie(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    title: so.Mapped[str] = so.mapped_column(sa.String(255), index=True)
+    genre: so.Mapped[str] = so.mapped_column(sa.String(50))
+    runtime: so.Mapped[str] = so.mapped_column(sa.String(25))
+    image_url: so.Mapped[str] = so.mapped_column(sa.String(255), index=True)
+    position: so.Mapped[str] = so.mapped_column(sa.String(10))
+    rating: so.Mapped[str] = so.mapped_column(sa.String(4))
+    box_office: so.Mapped[str] = so.mapped_column(sa.String(25))
+    director: so.Mapped[str] = so.mapped_column(sa.String(255))
+    release_date: so.Mapped[str] = so.mapped_column(sa.String(25))
+    budget: so.Mapped[str] = so.mapped_column(sa.String(25))
+    post_date: so.Mapped[date] =  so.mapped_column(sa.Date)
+    author_id = sa.Column(sa.Integer, sa.ForeignKey('User.id'), nullable=False)
+    author = so.relationship("User", foreign_keys=[author_id], primaryjoin="User.id == Movie.author_id")
