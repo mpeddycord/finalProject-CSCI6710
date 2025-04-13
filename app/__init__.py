c2a91f4a3d581df52fe812461c50c831d2f1b4a6
Michael Peddycord <michaelpeddycord@uncc.edu>
2025-04-12 21:00:40 -0400

    patch

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_bootstrap import Bootstrap4
 from config import Config
 from elasticsearch import Elasticsearch
+
+bootstrap = Bootstrap4()
 
 db = SQLAlchemy()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-
+    bootstrap.init_app(app)
     db.init_app(app)
 
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) if app.config['ELASTICSEARCH_URL'] else None
